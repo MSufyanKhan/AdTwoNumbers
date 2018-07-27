8022238ebeb4da0faefc7508ca9bb24c7d5a69a2
MSufyanKhan <41492320+MSufyanKhan@users.noreply.github.com>
2018-07-27 08:15:34 -0700

    Add files via upload

---
+++ AddTwoNumber/src/AddNumber.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main(args: Array<String>)
+{
+    print("Enter a first number which you have to add: ")
+    var sc = Scanner(System.`in`)
+    var first = sc.nextInt()
+    print("Enter a second number which you have to add: ")
+    var sec = sc.nextInt()
+    var sum:Int = first+sec
+    print("The sum of numbers is $sum ")
+
+
+}
